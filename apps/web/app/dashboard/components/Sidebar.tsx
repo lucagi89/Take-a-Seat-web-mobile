704bebffb9b194d03334fe37b86839af11eceb01
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2025-05-25 17:05:29 +0100

    restaurant dashboard

---
+++ apps/web/app/dashboard/components/Sidebar.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { useEffect, useState } from "react";
+import Styles from "../../../styles/dashboard.module.scss";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useUser } from "../../../contexts/userContext";
+import { getUserRestaurants } from "../../../lib/databaseActions";
+import { Restaurant } from "../../../data/types";
+
+export default function Sidebar() {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const [userRestaurants, setUserRestaurants] = useState<Restaurant[]>([]);
+
+  useEffect(() => {
+    if (user) {
+      const fetchRestaurants = async () => {
+        try {
+          const restaurants = await getUserRestaurants(user.uid);
+          console.log("Restaurants:", restaurants);
+          setUserRestaurants(restaurants);
+        } catch (error) {
+          console.error("Error fetching restaurants:", error);
+        }
+      };
+      fetchRestaurants();
+    }
+  }, [user]);
+  return (
+    <div className={Styles.sidebar}>
+      <h2 className="">Your Restaurants</h2>
+      {userRestaurants ? (
+        <ul className={Styles.restaurantList}>
+          {userRestaurants.map((restaurant) => (
+            <li key={restaurant.id} className={Styles.restaurantItem}>
+              <Link
+                href={`/restaurants/${restaurant.id}`}
+                className={Styles.restaurantLink}
+              >
+                <h3>{restaurant.name}</h3>
+                <p>{restaurant.description}</p>
+                <hr />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>
+          <p className="text-center">No restaurants found.</p>
+          <p className="text-center">
+            If you don&#39;t have any restaurants, you can create one.
+          </p>
+        </div>
+      )}
+      <button
+        onClick={() => router.push("/create-restaurant")}
+        className={Styles.button}
+      >
+        +
+      </button>
+    </div>
+  );
+}
--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,17 +1,12 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useUser } from "../../contexts/userContext";
 import Styles from "../../styles/dashboard.module.scss";
-import { getUserRestaurants } from "../../lib/databaseActions";
-import { Restaurant } from "../../data/types";
-import Link from "next/link";
 
 export default function DashboardPage() {
   const router = useRouter();
   const { user, loading } = useUser();
-
-  const [userRestaurants, setUserRestaurants] = useState<Restaurant[]>([]);
 
   useEffect(() => {
     if (!user && !loading) {
@@ -19,23 +14,8 @@ export default function DashboardPage() {
     }
   }, [user, loading, router]);
 
-  useEffect(() => {
-    if (user) {
-      const fetchRestaurants = async () => {
-        try {
-          const restaurants = await getUserRestaurants(user.uid);
-          console.log("Restaurants:", restaurants);
-          setUserRestaurants(restaurants);
-        } catch (error) {
-          console.error("Error fetching restaurants:", error);
-        }
-      };
-      fetchRestaurants();
-    }
-  }, [user]);
-
   return (
-    <main className={Styles.container}>
+    <main className={Styles.dashboardContainer}>
       {loading ? (
         <p className="text-center">Loading...</p>
       ) : user ? (
@@ -43,37 +23,6 @@ export default function DashboardPage() {
       ) : (
         <p className="text-center">Please log in to access your dashboard.</p>
       )}
-
-      <h2 className="">Your Restaurants</h2>
-      {userRestaurants ? (
-        <ul className={Styles.restaurantList}>
-          {userRestaurants.map((restaurant) => (
-            <li key={restaurant.id} className={Styles.restaurantItem}>
-              <Link
-                href={`/restaurants/${restaurant.id}`}
-                className={Styles.restaurantLink}
-              >
-                <h3>{restaurant.name}</h3>
-                <p>{restaurant.description}</p>
-                <hr />
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <div>
-          <p className="text-center">No restaurants found.</p>
-          <p className="text-center">
-            If you don&#39;t have any restaurants, you can create one.
-          </p>
-        </div>
-      )}
-      <button
-        onClick={() => router.push("/create-restaurant")}
-        className={Styles.button}
-      >
-        Create New Restaurant
-      </button>
       <h2 className="">Go to Home</h2>
     </main>
   );
